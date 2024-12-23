a9c631573bc54475c8b40402ee91dd1194ddda88
NaderWD <Nader.webdevop@gmail.com>
2024-12-23 11:39:02 +0330

    Application Registration

---
+++ NaderE-Shop.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace NaderE_Shop.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static void ConfigureApplicationService(this IServiceCollection services)
+        {
+            //services.AddAutoMapper(typeof(MappingProfile));
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
