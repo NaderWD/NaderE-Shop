ae7e4de662b5f56e9c8e38515a1163ac288202fe
NaderWD <Nader.webdevop@gmail.com>
2024-12-23 12:18:43 +0330

    Infrastructure And Persistence Services Registration

---
+++ NaderE-Shop.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NaderE_Shop.Application.Contracts.InfrastructureCont;
+using NaderE_Shop.Application.OtherModels.Email;
+using NaderE_Shop.Infrastructure.Mail;
+
+namespace NaderE_Shop.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureService(this IServiceCollection service, IConfiguration configuration)
+        {
+            var section = configuration.GetSection("EmailSettings");
+            service.Configure<EmailSetting>(section);
+
+            service.AddTransient<IEmailSender, EmailSender>();
+
+            return service;
+        }
+    }
+}
---
+++ NaderE-Shop.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NaderE_Shop.Application.Contracts.PersistenceCont;
+using NaderE_Shop.Persistence.Repositories;
+
+namespace NaderE_Shop.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServis(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connection = configuration.GetConnectionString("ShopDbConnection");
+            services.AddDbContext<ShopDbContext>(options => options.UseSqlServer(connection));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped(typeof(ICategoryRepository), typeof(CategoryRepository));
+            services.AddScoped(typeof(ICommentRepository), typeof(CommentAttribute));
+            services.AddScoped(typeof(ICustomFormatter), typeof(CustomerRepository));
+            services.AddScoped(typeof(IOrderRepository), typeof(OrderRepository));
+            services.AddScoped(typeof(IProductRepository), typeof(ProductRepository));
+            services.AddScoped(typeof(IShoppingCartRepository), typeof(ShoppingCartRepository));
+            services.AddScoped(typeof(IShopUserRepository), typeof(ShopUserRepository));
+
+            return services;
+        }
+    }
+}
