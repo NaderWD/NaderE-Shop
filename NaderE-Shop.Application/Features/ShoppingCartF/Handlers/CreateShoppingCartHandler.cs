00c18d12884cbe7cd5e9072ca13fed35b1113603
NaderWD <Nader.webdevop@gmail.com>
2024-12-23 12:07:22 +0330

    Email Sender In Handler

--- NaderE-Shop.Application/Features/ShoppingCartF/Handlers/CreateShoppingCartHandler.cs
+++ NaderE-Shop.Application/Features/ShoppingCartF/Handlers/CreateShoppingCartHandler.cs
@@ -1,20 +1,39 @@
 ﻿using AutoMapper;
 using MediatR;
+using NaderE_Shop.Application.Contracts.InfrastructureCont;
 using NaderE_Shop.Application.Contracts.PersistenceCont;
 using NaderE_Shop.Application.Features.ShoppingCartF.Requests;
+using NaderE_Shop.Application.OtherModels.Email;
 using NaderE_Shop.Domain.Models;
 
 namespace NaderE_Shop.Application.Features.ShoppingCartF.Handlers
 {
-    public class CreateShoppingCartHandler(IShoppingCartRepository repository, IMapper mapper) : IRequestHandler<CreateShoppingCartRequest, int>
+    public class CreateShoppingCartHandler(IShoppingCartRepository repository, IMapper mapper, IEmailSender emailSender) : IRequestHandler<CreateShoppingCartRequest, int>
     {
         private readonly IShoppingCartRepository _repository = repository;
         private readonly IMapper _mapper = mapper;
+        private readonly IEmailSender _emailSender = emailSender;
 
         public async Task<int> Handle(CreateShoppingCartRequest request, CancellationToken cancellationToken)
         {
             var cart = _mapper.Map<ShoppingCart>(request.CreateShoppingCart);
             await _repository.Add(cart);
+
+            var email = new Email
+            {
+                To = "Customer@gmail.com",
+                Subject = "Shopping Cart Creation",
+                Body = $"Your New Shopping Cart Has Been Created in {request.CreateShoppingCart.AddedDate}",
+            };
+            try
+            {
+                await _emailSender.SendEmail(email);
+            }
+            catch (Exception)
+            {
+                //log
+            }
+
             return cart.Id;
 
         }
---
+++ NaderE-Shop.Infrastructure/Mail/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using NaderE_Shop.Application.Contracts.InfrastructureCont;
+using NaderE_Shop.Application.OtherModels.Email;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net;
+
+namespace NaderE_Shop.Infrastructure.Mail
+{
+    public class EmailSender(EmailSetting emailSetting) : IEmailSender
+    {
+        private readonly EmailSetting _emailSetting = emailSetting;
+
+        public async Task<bool> SendEmail(Email email)
+        {
+            var client = new SendGridClient(_emailSetting.ApiKey);
+            var to = new EmailAddress(email.To);
+            var from = new EmailAddress()
+            {
+                Name = _emailSetting.FromName,
+                Email = _emailSetting.FromAddress,
+            };
+            var message = MailHelper.CreateSingleEmail(from, to, email.Subject, email.Body, email.Body);
+            var response = await client.SendEmailAsync(message);
+
+            return response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Accepted;
+        }
+    }
+}
