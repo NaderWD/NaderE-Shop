b1c732147bfa9d61ad13a57e2c32634b79cd5863
NaderWD <Nader.webdevop@gmail.com>
2024-12-23 08:32:03 +0330

    DbContext

---
+++ NaderE-Shop.Persistence/ShopDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NaderE_Shop.Domain.Models;
+
+namespace NaderE_Shop.Persistence
+{
+    public class ShopDbContext(DbContextOptions<ShopDbContext> options) : DbContext(options)
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<ShopUser> ShopUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ShopDbContext).Assembly);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries<Base>())
+            {
+                entry.Entity.AddedDate = DateTime.Now;
+            }
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<Base>())
+            {
+                entry.Entity.AddedDate = DateTime.Now;
+            }
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    }
+}
