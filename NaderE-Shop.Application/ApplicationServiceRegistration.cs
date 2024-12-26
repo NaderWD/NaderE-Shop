3c3c2046705e84db453b213071af54616d3ac3ff
NaderWD <Nader.webdevop@gmail.com>
2024-12-26 17:35:47 +0330

    MediatR Added In Application Registration Service Class

--- NaderE-Shop.Application/ApplicationServiceRegistration.cs
+++ NaderE-Shop.Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace NaderE_Shop.Application
@@ -9,6 +10,7 @@ namespace NaderE_Shop.Application
         {
             //services.AddAutoMapper(typeof(MappingProfile));
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
         }
     }
 }
